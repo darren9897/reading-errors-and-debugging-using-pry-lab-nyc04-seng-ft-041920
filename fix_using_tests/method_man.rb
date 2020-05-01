eb0b5d32c74615af2fcd7be877e4dcf82cb0bbd5
Darren Koomson <darrenkoomson17@gmail.com>
2020-05-01 00:23:57 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,7 +1,8 @@
 # don't forget to add: require 'pry'
 
 def start_game(player1, player2)
-  p "Hello #{player1} & #{playar2}"
+  greeting = "Hello #{player1} & #{playar2}"
+  return greeting
 end
 
 def play_game(player1, player2)
